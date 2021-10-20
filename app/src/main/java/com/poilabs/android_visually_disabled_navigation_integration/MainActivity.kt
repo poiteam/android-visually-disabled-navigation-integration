a94f6cc6789dd1bf0c4a978524aea9b79cf54e42
Mohammad Khakpaki <mohammad@poilabs.com>
2021-10-20 12:59:03 +0300

    Add project

---
+++ app/src/main/java/com/poilabs/android_visually_disabled_navigation_integration/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.poilabs.android_visually_disabled_navigation_integration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
